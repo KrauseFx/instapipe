9f1f76dcef4146bf5d30c33557ad9d27a38fc50f
Felix Krause <krausefx@gmail.com>
2019-03-11 14:24:52 +0900

    Preloading of stories, hiding of story indicator, fix of preloading last story

--- web/instapipe.js
+++ web/instapipe.js
@@ -3,53 +3,71 @@ var timeOutForPhotos = 4.0;
 var storyProgressSpacing = 5;
 var progressPadding = 2;
 var progressBars = null;
+var storiesContent = null;
 
 var nextStoryTimeout = null;
 var currentIndex = -1;
 
-function showStories() {
+function preloadStoriesIndex() {
   var url = "https://instapipe.herokuapp.com/stories.json";
-  // url = "http://127.0.0.1:4567/stories.json"
 
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function() { 
     if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      var content = JSON.parse(xmlHttp.responseText)
-      storiesToShow = []
-      progressBars = []
-
-      document.getElementById("storyViewer").style.display = "block"
-
-      for (let storyIndex in content) {
-        let currentStory = content[storyIndex]
-        storiesToShow.push(currentStory)
+      storiesContent = JSON.parse(xmlHttp.responseText)
+      if (storiesContent.length == 0) {
+        // Default it's shown, as it looks nicer
+        // and I post stories most days :joy:
+        document.getElementById("story-available").style.display = "none"
+      } else {
+        // preload the first story if it's a picture
+        if (!storiesContent[0]["is_video"]) {
+          document.getElementById("fakeContentToPreloadImages").src = storiesContent[0]["signed_url"]
+        }
       }
-
-      for (let currentStoryIndex in storiesToShow) {
-        let currentStory = storiesToShow[currentStoryIndex]
-
-        // Append the progress items
-        var progressBarBackground = document.createElement("div")
-        progressBarBackground.className = "storyProgressBarItemBg"
-        progressBarBackground.style.width = "calc(" + (1.0 / storiesToShow.length) * 100 + "%" + " - " + progressPadding * 2 + "px)"
-        progressBarBackground.style.marginRight = progressPadding + "px"
-        progressBarBackground.style.marginLeft = progressPadding + "px"
-        document.getElementById("storyProgressBar").appendChild(progressBarBackground)
-
-        var progressBarForeground = document.createElement("div")
-        progressBarForeground.style.width = "0%"
-        progressBarForeground.className = "storyProgressBarItemFg"
-        progressBarBackground.appendChild(progressBarForeground)
-
-        progressBars.push(progressBarForeground)
-      }
-
-      currentIndex = 0
-      renderCurrentStory()
     }
-  }
+  };
+
   xmlHttp.open("GET", url, true); // true = asynchronous 
   xmlHttp.send(null);
+}
+
+function showStories() {
+  if (storiesContent == null || storiesContent.length == 0) {
+    return;
+  }
+
+  storiesToShow = []
+  progressBars = []
+
+  document.getElementById("storyViewer").style.display = "block"
+
+  for (let storyIndex in storiesContent) {
+    let currentStory = storiesContent[storyIndex]
+    storiesToShow.push(currentStory)
+  }
+
+  for (let currentStoryIndex in storiesToShow) {
+    let currentStory = storiesToShow[currentStoryIndex]
+
+    // Append the progress items
+    var progressBarBackground = document.createElement("div")
+    progressBarBackground.className = "storyProgressBarItemBg"
+    progressBarBackground.style.width = "calc(" + (1.0 / storiesToShow.length) * 100 + "%" + " - " + progressPadding * 2 + "px)"
+    progressBarBackground.style.marginRight = progressPadding + "px"
+    progressBarBackground.style.marginLeft = progressPadding + "px"
+    document.getElementById("storyProgressBar").appendChild(progressBarBackground)
+
+    var progressBarForeground = document.createElement("div")
+    progressBarForeground.style.width = "0%"
+    progressBarForeground.className = "storyProgressBarItemFg"
+    progressBarBackground.appendChild(progressBarForeground)
+
+    progressBars.push(progressBarForeground)
+  }
+
+  currentIndex = 0
+  renderCurrentStory()
 }
 
 function renderCurrentStory() {
@@ -104,7 +122,7 @@ function renderCurrentStory() {
   }
 
   // Trigger the next one
-  if (currentIndex < storiesToShow.length - 2 && !storiesToShow[currentIndex + 1]["is_video"])
+  if (currentIndex < storiesToShow.length - 1 && !storiesToShow[currentIndex + 1]["is_video"])
   {
     setTimeout(function() {
       // Poor person's pre-loading of images, with a slight delay
@@ -159,4 +177,4 @@ window.addEventListener("keyup", function(e) {
   }
 }, false);
 
-// showStories()
+preloadStoriesIndex();
